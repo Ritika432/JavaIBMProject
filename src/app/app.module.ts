f7af46f308853fb93b176b120895db4b35a78ab6
Ritika432 <ritika.4342@gmail.com>
2020-05-04 13:53:09 +0530

    Vehicle Services Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { Injectable } from '@angular/core';
+
 import {FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AdminComponentComponent } from './admin-component/admin-component.component';
-import { UserComponentComponent } from './user-component/user-component.component';
-import { CreditCardComponentComponent } from './credit-card-component/credit-card-component.component';
-import { LoginComponentComponent } from './login-component/login-component.component';
-import { RouteComponentComponent } from './route-component/route-component.component';
-import { ServicesComponentComponent } from './services-component/services-component.component';
-import { BookingComponentComponent } from './booking-component/booking-component.component';
-import { AdminComponent } from './admin/admin.component';
-import { UserComponent } from './user/user.component';
-import { CreditCardComponent } from './credit-card/credit-card.component';
-import { BookingComponent } from './booking/booking.component';
-import { RouteComponent } from './route/route.component';
 import { VehicleComponent } from './vehicle/vehicle.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
-import { RouteListComponent } from './route-list/route-list.component';
+
+
+
 
 
 
@@ -32,23 +19,9 @@ import { RouteListComponent } from './route-list/route-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponentComponent,
-    UserComponentComponent,
-    CreditCardComponentComponent,
-    LoginComponentComponent,
-    RouteComponentComponent,
-    ServicesComponentComponent,
-    BookingComponentComponent,
-    AdminComponent,
-    UserComponent,
-    CreditCardComponent,
-    BookingComponent,
-    RouteComponent,
-    VehicleComponent,
-    LoginComponent,
-    RegistrationComponent,
-    VehicleListComponent,
-    RouteListComponent,
+    VehicleComponent
+
+    
 
   ],
   imports: [
@@ -56,7 +29,7 @@ import { RouteListComponent } from './route-list/route-list.component';
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    //Injectable,
+   
     HttpClientModule,
     BrowserAnimationsModule
   ],
