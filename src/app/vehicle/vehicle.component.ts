f0d580ee8ed1f03f6d67f62c4d0c14196bba0e75
saismaran <saismaranhotaeducation@gmail.com>
2020-05-06 01:18:21 +0530

    Final Table Template commit

--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -6,23 +6,38 @@ import { VehicleService } from '../services/vehicle.service';
 
 @Component({
   selector: 'appvehicle',
-  template:`<div>
-  <h1>
-  {{ vehicles | json }}
-  </h1>
-  </div>`,
-   providers:[VehicleService]
-  })
-  export class VehicleComponent 
-  {
-     public vehicles;
-     constructor(private vehicleService:VehicleService)
-     {
-       this.vehicleService.getVehicle().subscribe((response)=>
-       {
-        this.vehicles=response;console.log('Received vehicles:',response);
-         }
-       
-       );
-     }
+  // template:`<div>
+  // <h1>
+  // {{ vehicles | json }}
+  // </h1>
+  // </div>`,
+  templateUrl: "./vehicle.component.html",
+  styleUrls: ["./vehicle.component.css"],
+  providers: [VehicleService]
+})
+export class VehicleComponent {
+  public vehicles = [];
+  constructor(private vehicleService: VehicleService) {
+    this.vehicleService.getVehicle().subscribe((response) => {
+      console.log("response array", response)
+      let key;
+      for(key in response){
+        this.vehicles.push(response[key]);
+      }
+
+      console.log("vehicle",this.vehicles)
+
+    }
+
+
+
+    );
   }
+  
+}
+
+      // this.temp.push(response);
+      // for (let index = 0; index < this.temp.length*2; index++) {
+      //   let x= this.temp[index];
+      //   this.vehicles.push(x[index]);
+      // }
