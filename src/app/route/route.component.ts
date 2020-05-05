d2c87fe6ca51a36c268ac08ef16971d9905e72c3
Anurag-hub <60914815+Anurag-hub@users.noreply.github.com>
2020-05-06 01:56:53 +0530

    route ui done(get method)

--- src/app/route/route.component.ts
+++ src/app/route/route.component.ts
@@ -4,27 +4,41 @@ import { RouteService } from '../services/route.services';
 //import {Pipe,PipeTransform} from '@angular/core';
 
 
-
 @Component({
   selector: 'approute',
-  template:`<div>
-  <h1>
-  {{ route | json }}
-  </h1>
-  </div>`,
-   providers:[RouteService]
-  })
-  export class RouteComponent 
-  {
-     public route;
-     constructor(private routeService:RouteService)
-     {
-       this.routeService.getRouteDetails().subscribe((response)=>
-       {
-        this.route=response;
-        console.log('Received route details:',response);
-         }
-       
-       );
-     }
+  //template:`<div>
+  //<h1>
+  //{{ route | json }}
+  //</h1>
+  //</div>`,
+  templateUrl: "./route.component.html",
+  styleUrls: ["./route.component.css"],
+  providers: [RouteService]
+})
+export class RouteComponent  {
+  public routes = [];
+  constructor(private routeService:RouteService) {
+    
+    this.routeService.getRouteDetails().subscribe((response) => {
+      console.log("response array", response)
+      let key;
+      for(key in response){
+        this.routes.push(response[key]);
+      }
+
+      console.log("vehicle",this.routes)
+
+    }
+
+
+
+    );
   }
+  
+}
+
+      // this.temp.push(response);
+      // for (let index = 0; index < this.temp.length*2; index++) {
+      //   let x= this.temp[index];
+      //   this.vehicles.push(x[index]);
+      // }
