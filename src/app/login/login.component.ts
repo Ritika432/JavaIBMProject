acad4a7bac20cebcd39b28d5c541969443279d58
Shubham Agrawal <shubh.agrawal12@gmail.com>
2020-05-05 19:42:23 +0530

    login page commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoginService } from '../services/login.service'
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  providers:  [ LoginService ],
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+ login : LoginComponent = new LoginComponent();
 
-  ngOnInit(): void {
+
+  
+  constructor(public  loginService:  LoginService) 
+  
+  {
+   
   }
 
+ addUser(): void {
+
+
+  this.loginService.getUser(this.login)
+  .subscribe(data => { alert ("Login successfully");
+});
+
+
+
+ }
+
+  ngOnInit(): void {
+   
+  }
+
+ 
 }
