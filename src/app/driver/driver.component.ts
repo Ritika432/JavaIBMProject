69739e381c76de20d66a7a8b8c834838031abcfe
Ritika432 <ritika.4342@gmail.com>
2020-05-06 11:09:40 +0530

    Driver Component

--- src/app/driver/driver.component.ts
+++ src/app/driver/driver.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+export class Driver {
+public driverId: String;
+public name: string;
+public street: string;
+public location: string;
+public city: string;
+public state: string;
+public pincode: string;
+public mobileNumber: Number;
+public licenseNumber: string;
+}
 @Component({
-  selector: 'app-driver',
-  templateUrl: './driver.component.html',
-  styleUrls: ['./driver.component.css']
+selector: 'register-driver',
+templateUrl: './driver.component.html',
+//styleUrls: ['./app.component.css']
+providers: [ AdminService ]
 })
-export class DriverComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DriverComponent {
+public driver;
+model = new Driver();
+public getRegistrationservice;
+constructor(private getService: AdminService)
+{
+this.getRegistrationservice=getService;
+}
+ onSubmit(name:any,street:any,location:any,city:any,state:any,pincode:any,mobile:any,license:any)
+{
+this.getRegistrationservice.Driver({
+"name":name.value,
+"street":street.value,
+"location":location.value,
+"city":city.value,
+"state":state.value,
+"pincode":pincode.value,
+"mobileNo":mobile.value,
+"licenseNumber":license.value
+}
+).subscribe((response)=> {
+this.driver=response;
+console.log("New Driver added : ",response);
+});
+}
 }
