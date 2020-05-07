f6520fcc40b62c48c78c6d575399501adef47ea1
saismaran <saismaranhotaeducation@gmail.com>
2020-05-08 04:11:29 +0530

    StableCode v.1

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service'
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  providers:  [ AdminService ],
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  vehiclelist : Observable <Object>;
+  vehicleid : number;
+  
+  constructor(public adminService: AdminService) 
+  
+  {
+   
+  }
+
+  ngOnInit(): void
+   {
+    
+  }
+
+ 
+  getData() {
+    
+    this.vehiclelist=this.adminService.getVehicle(this.vehicleid);
+
+  }
+
+}
