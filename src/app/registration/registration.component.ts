0e1329327c0fd7e6f157fb2bc9f8e501f44c14c5
saismaran <saismaranhotaeducation@gmail.com>
2020-05-07 17:15:25 +0530

    trash

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
-  templateUrl: '',
-  styleUrls: ['']
+  templateUrl: 'registration.component.html',
+  styleUrls: ['./registration.component.css']
 })
 
 export class RegistrationComponent implements OnInit {
